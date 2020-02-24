ce4afa56b501d1eb16904cd22b29b9f1f59aca51
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2020-02-23 19:24:54 -0800

    adding files from local to github repository

---
+++ app.js
@@ -0,0 +1,31 @@
+$(() => {
+
+        $('form').on('submit', () => {
+            const $item = $('#input-box').val();
+            event.preventDefault();
+            console.log($item);
+
+            const $firstDiv = $('<li>').addClass('todo').html('<h2>'+$item+'</h2>');
+
+
+            const $firstbutton =
+            $('<button>').text('Mark Complete').appendTo($firstDiv);
+
+            $('.ul1').append($firstDiv);
+
+
+            $firstbutton.on('click', () => {
+                $('#completed').append($firstDiv);
+                $firstDiv.css('background-color','#FCF2F6');
+                $firstbutton.addClass('remove').text('Remove Item');
+
+
+                $('.remove').on('click', (event) => {
+                    event.target.closest('li').remove();
+                });
+            });
+
+
+        });
+
+});
